c6c7b20d64881b335f9dd4b98c58018b8ff4b848
Andrey <andrey.wys@gmail.com>
2021-12-16 12:05:52 +0300

    Add Ubiquiti EdgeSwitch

--- backupscript.sh
+++ backupscript.sh
@@ -4,7 +4,7 @@ USER_FTP=admin
 PASSWD_FTP=admin
 HOST_FTP=192.168.1.1
 PORT_FTP=21
-RFOLDER="Backups/Dlink/"
+RFOLDER="Automation/Backups/Dlink/"
 LOG="/tmp/`basename $BASH_SOURCE | cut -f 1 -d '.'`.log"
 echo -n >  $LOG
 USER=admin
@@ -30,6 +30,8 @@ REQ_IP=()
 FIRST_IP=192.168.1.1
 NUM_IP=100
 
+rm -f $LOG
+rm -f /temp/temp.txt
 
 if ! dpkg -s tftpd-hpa > /dev/null; then
     echo "tftp-hpa is not installed"
@@ -55,11 +57,11 @@ fi
 
 function SendtoFTP(){
     local ret=0
-    if [ $# -eq 2 ]; then
-	if [ -s $TFTPROOT/$1.cfg ]; then
-	    mv $TFTPROOT/$1.cfg $TFTPROOT/$DATE-$1.cfg
-	    curl -# --upload-file "$TFTPROOT/$DATE-$1.cfg" ftp://$USER_FTP:$PASSWD_FTP@$HOST_FTP:$PORT_FTP/$2 && \
-	    rm $TFTPROOT/$DATE-$1.cfg >> $LOG
+    if [ $# -eq 3 ]; then
+	if [ -s $TFTPROOT/$1.$3 ]; then
+	    mv $TFTPROOT/$1.$3 $TFTPROOT/$DATE-$1.$3
+	    curl -# --upload-file "$TFTPROOT/$DATE-$1.$3" ftp://$USER_FTP:$PASSWD_FTP@$HOST_FTP:$PORT_FTP/$2 && \
+	    rm $TFTPROOT/$DATE-$1.$3 >> $LOG
 	    ret=0
 	else
 	    ret=2
@@ -94,12 +96,12 @@ echo $ret
 
 function SSHRequest(){
     local ret=0
-    if [ $# -eq 3 ]; then
+    if [ $# -eq 4 ]; then
 	if ! grep "$(ssh-keyscan $ip 2>/dev/null)" ~/.ssh/known_hosts > /dev/null; then
 	    echo "Unknow ssh host $1" >> $LOG 
 	    ssh-keyscan $1 >> ~/.ssh/known_hosts
 	fi
-	sshpass -p $PASSWD scp $USER@$1:$2 $3/$1.cfg
+	sshpass -p $PASSWD scp $USER@$1:$2 $3/$1.$4
     ret=0
     else
 	echo "Wrong parameters in function SSHRequest" >> $LOG
@@ -157,7 +159,7 @@ do
 		.1.3.6.1.4.1.171.14.14.1.2.1.5.1 i 1 \
 		.1.3.6.1.4.1.171.14.14.1.2.1.6.1 x $TFTP_SERV_HEX \
 		.1.3.6.1.4.1.171.14.14.1.2.1.12.1 i 4 >>$LOG
-	    RFOLDER="Backups/Dlink/"
+	    RFOLDER="Automation/Backups/Dlink/"
 	;;
 
 	"WS6-DGS-1210-26/F1"|"WS6-DGS-1210-10P/F1"|"WS6-DGS-1210-28P/F1"|"WS6-DGS-1210-20/F1"|"WS6-DGS-1210-52/F1"|"WS6-DGS-1210-28/F1" )
@@ -167,93 +169,131 @@ do
 	    # 4 - Filename
 	    # 5 - Operation status 1-download, 2 - upload
 	    # 6 - Config ID (1,2)
+	    EXTFILE="cfg"
 	    $SNMPSET $ip \
 		.1.3.6.1.4.1.171.11.153.1000.3.10.5.0 i 2 \
-		.1.3.6.1.4.1.171.11.153.1000.3.10.4.0 s "$ip.cfg" \
+		.1.3.6.1.4.1.171.11.153.1000.3.10.4.0 s "$ip.$EXTFILE" \
 		.1.3.6.1.4.1.171.11.153.1000.3.10.1.0 x $TFTP_SERV_HEX \
 		.1.3.6.1.4.1.171.11.153.1000.3.10.2.0 i 1 >> $LOG
-	    RFOLDER="Backups/Dlink/"
+	    RFOLDER="Automation/Backups/Dlink/"
 	;;
 
 	"DGS-1210-52/ME/A1" )
+	    EXTFILE="cfg"
 	    $SNMPSET $ip \
 		.1.3.6.1.4.1.171.10.76.29.1.3.10.1.0 x $TFTP_SERV_HEX \
-		.1.3.6.1.4.1.171.10.76.29.1.3.10.4.0 s "$ip.cfg" \
+		.1.3.6.1.4.1.171.10.76.29.1.3.10.4.0 s "$ip.$EXTFILE" \
 		.1.3.6.1.4.1.171.10.76.29.1.3.10.5.0 i 2 \
 		.1.3.6.1.4.1.171.10.76.29.1.3.10.7.0 i 1 >>$LOG
-	    RFOLDER="Backups/Dlink/"
+	    RFOLDER="Automation/Backups/Dlink/"
 	;;
 
 	"DGS-1210-10P" )
+
+	    EXTFILE="cfg"
 	    $SNMPSET $ip \
 		.1.3.6.1.4.1.171.10.76.12.3.5.0 a $TFTP_SERV \
-		.1.3.6.1.4.1.171.10.76.12.3.6.0 s "$ip.cfg" \
-		.1.3.6.1.4.1.171.10.76.12.3.7.0 i 2  >>$LOG
-	    RFOLDER="Backups/Dlink/"
+		.1.3.6.1.4.1.171.10.76.12.3.6.0 s  "$ip.$EXTFILE" \
+		.1.3.6.1.4.1.171.10.76.12.3.7.0 i 2  >> $LOG
+	    RFOLDER="Automation/Backups/Dlink/"
+
+#	    $SNMPSET $ip \
+#		.1.3.6.1.4.1.171.10.76.28.1.3.10.1.0 s $TFTP_SERV \
+#		.1.3.6.1.4.1.171.10.76.28.1.3.10.2.0 i 1 \
+#		.1.3.6.1.4.1.171.10.76.28.1.3.10.4.0 s "$ip.cfg" \
+#		.1.3.6.1.4.1.171.10.76.28.1.3.10.7.0 i 1 \
+#		.1.3.6.1.4.1.171.10.76.28.1.3.10.5.0 i 2  >>$LOG
+#	    RFOLDER="Automation/Backups/Dlink/"
 	;;
 
+
 	"DGS-1210-52/C1" )
+	    EXTFILE="cfg"
 	    $SNMPSET $ip \
 		.1.3.6.1.4.1.171.10.76.22.1.3.10.1.0 x $TFTP_SERV_HEX \
-		.1.3.6.1.4.1.171.10.76.22.1.3.10.4.0 s "$ip.cfg" \
+		.1.3.6.1.4.1.171.10.76.22.1.3.10.4.0 s "$ip.$EXTFILE" \
 		.1.3.6.1.4.1.171.10.76.22.1.3.10.5.0 i 2  >>$LOG
-	    RFOLDER="Backups/Dlink/"
+	    RFOLDER="Automation/Backups/Dlink/"
 	;;
 
 	"DGS-1210-10P/C1" )
+	    EXTFILE="cfg"
 	    $SNMPSET $ip \
 		.1.3.6.1.4.1.171.10.76.18.1.3.10.1.0 x $TFTP_SERV_HEX \
-		.1.3.6.1.4.1.171.10.76.18.1.3.10.4.0 s "$ip.cfg" \
+		.1.3.6.1.4.1.171.10.76.18.1.3.10.4.0 s "$ip.$EXTFILE" \
 		.1.3.6.1.4.1.171.10.76.18.1.3.10.5.0 i 2  >>$LOG
-	    RFOLDER="Backups/Dlink/"
+	    RFOLDER="Automation/Backups/Dlink/"
 	;;
 
 	"DGS-1210-28" )
+	    EXTFILE="cfg"
 	    $SNMPSET $ip \
 		.1.3.6.1.4.1.171.10.76.15.3.10.1 a $TFTP_SERV \
-		.1.3.6.1.4.1.171.10.76.15.3.10.4 s "$ip.cfg" \
+		.1.3.6.1.4.1.171.10.76.15.3.10.4 s "$ip.$EXTFILE" \
 		.1.3.6.1.4.1.171.10.76.15.3.10.5 i 2  >>$LOG
-	    RFOLDER="Backups/Dlink/"
+	    RFOLDER="Automation/Backups/Dlink/"
 
 ;;
 
 	# Ubiquiti
 	"Linux" )
-	    LFILE_SSH=/tmp/system.cfg
+	    EXTFILE="cfg"
+	    LFILE_SSH=/tmp/system.$EXTFILE
 	    RFOLDER_SSH=/srv/tftp
-	    RFOLDER="Backups/UBNT/"
-	    SSHRequest $ip $LFILE_SSH $RFOLDER_SSH
+	    RFOLDER="Automation/Backups/UBNT/"
+	    SSHRequest $ip $LFILE_SSH $RFOLDER_SSH $EXTILE
+	;;
+
+
+	# Ubiquiti EdgeSwitch fw > 2.1
+	"EdgeSwitch" )
+
+	    EXTFILE="tar.gz"
+	    LFILE_SSH=/tmp/backup.$EXTFILE
+	    SSHCOMMAND="/bin/backupcfg encode edgeswitchbackup -f > $LFILE_SSH"
+	    sshpass -p $PASSWD ssh $USER@$ip $SSHCOMMAND
+	    RFOLDER_SSH=/srv/tftp
+	    RFOLDER="Automation/Backups/UBNT/"
+	    SSHRequest $ip $LFILE_SSH $RFOLDER_SSH $EXTFILE
 	;;
 
 	"DFL-260E" )
-	    LFILE_SSH=system.bak
+
+	    EXTFILE="bak"
+	    LFILE_SSH=system.$EXTFILE
 	    RFOLDER_SSH=/srv/tftp
-	    RFOLDER="Backups/DFL/"
-	    SSHRequest $ip $LFILE_SSH $RFOLDER_SSH
+	    RFOLDER="Automation/Backups/DFL/"
+	    EXTFILE="bak"
+	    SSHRequest $ip $LFILE_SSH $RFOLDER_SSH $EXTFILE
 	;;
 
 	"ZyXEL"|"Keenetic" )
+	    EXTFILE="cfg"
 	    REQUESTSTRING="cat startup-config"
-	    RFOLDER="Backups/Keenetic/"
+	    RFOLDER="Automation/Backups/Keenetic/"
 	    OUT=/tmp/temp.txt
 	    echo -n > $OUT 
 	    TelnetRequest $ip "${REQUESTSTRING}" $OUT
-	    cat $OUT | sed 's/\r$//; 1,/name = startup-config:/d; /(config)>/,$d' > $TFTPROOT/$ip.cfg
+	    cat $OUT | sed 's/\r$//; 1,/name = startup-config:/d; /(config)>/,$d' > $TFTPROOT/$ip.$EXTFILE
 	;;
 
 	"DGS-1100-08P"|"192.168.1.13" )
-	    RFOLDER="Backups/Dlink/"
+
+	    EXTFILE="cfg"
+	    RFOLDER="Automation/Backups/Dlink/"
 	    COOKIE=(`curl -i -# -X POST -d pass=$PASSWD_MD5 http://$ip/cgi/login.cgi | sed 's|.*SessID=||; s|.*Gambit=||'|sed -r 's/;path.+//'|sed -n "2,3p"`)
-	    curl -X POST -d "pswType=1"  "http://$ip/cgi/backup.cgi" -H "Cookie: Gambit=${COOKIE[1]}; SessID=${COOKIE[0]}" -o $TFTPROOT/$ip.cfg
+	    curl -X POST -d "pswType=1"  "http://$ip/cgi/backup.cgi" -H "Cookie: Gambit=${COOKIE[1]}; SessID=${COOKIE[0]}" -o $TFTPROOT/$ip.$EXTFILE
 	;;
+	
 	# HP ProCurve 1810G - 24 GE
 	"192.168.1.6" )
-	    RFOLDER="Backups/Dlink/"
+	    EXTFILE="cfg"
+	    RFOLDER="Automation/Backups/Dlink/"
 	    COOKIE=`curl -i -# --cookie-jar - -o /dev/null -X POST -d \
 		"pwd=$PASSWD&login=Login&err_flag=&err_msg=" \
 		"http://$ip/hp_login.html"|sed 's|.*SID\t||'|\
 		sed -r 's/;\n+//'|sed -n '5p'`
-	    curl -i -# -o /dev/null -X POST -d "v_1_1_1=TFTP&v_1_2_1=TFTP&v_1_2_1=TFTP&v_1_3_1=IPv4&v_1_3_1=IPv4&v_1_4_1=$TFTP_SERV&v_1_4_1=$TFTP_SERV&v_1_5_1=$ip.cfg&v_1_5_1=$ip.cfg&v_1_6_1=Configuration&v_1_6_1=Configuration&v_1_16_2=1&v_1_14_1=image2&v_1_15_1=image1&v_1_10_1=1&v_2_1_1=%%A0&submit_flag=8&submit_target=transfer_in_progress.html&err_flag=0&err_msg=&clazz_information=UploadFile.html&v_1_11_1=Upload" \
+	    curl -i -# -o /dev/null -X POST -d "v_1_1_1=TFTP&v_1_2_1=TFTP&v_1_2_1=TFTP&v_1_3_1=IPv4&v_1_3_1=IPv4&v_1_4_1=$TFTP_SERV&v_1_4_1=$TFTP_SERV&v_1_5_1=$ip.$EXTFILE&v_1_5_1=$ip.$EXTFILE&v_1_6_1=Configuration&v_1_6_1=Configuration&v_1_16_2=1&v_1_14_1=image2&v_1_15_1=image1&v_1_10_1=1&v_2_1_1=%%A0&submit_flag=8&submit_target=transfer_in_progress.html&err_flag=0&err_msg=&clazz_information=UploadFile.html&v_1_11_1=Upload" \
 		"http://$ip/UploadFile.html" -H "Cookie: SID=$COOKIE; dw_nav=Maintenance"
 	    curl -i -# -o /dev/null -X POST -d "loginpage=hp_login.html" "http://$ip/links.html" \
 		    -H "Cookie: SID=$COOKIE"
@@ -267,10 +307,10 @@ do
     esac
     if [ $NOTFOUND -ne 1 ]; then
 	sleep 5
-	if [ $(SendtoFTP $ip $RFOLDER) -ne 0 ]; then
+	if [ $(SendtoFTP $ip $RFOLDER $EXTFILE) -ne 0 ]; then
 	    echo "File from $ip not ready. Try to waiting file..." >>$LOG
 	    sleep 10
-	    SendtoFTP $ip $RFOLDER
+	    SendtoFTP $ip $RFOLDER $EXTFILE
 	fi
     fi
 done
